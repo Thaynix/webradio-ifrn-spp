cd53ad1f05fd770336df9044cb2617ac0610853e
Thaynix <thaygoncalvesjg@gmail.com>
2024-03-13 23:24:24 -0300

    modificações no front

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector("#toggle-btn");
+
+toggleBtn.addEventListener("click", function (){
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+
