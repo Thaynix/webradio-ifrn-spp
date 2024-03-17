5716f745a8466d2d34325ef70675147dc8fd363d
Thaynix <thaygoncalvesjg@gmail.com>
2024-03-17 19:55:18 -0300

    Commit Inicial

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webradio',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -112,3 +118,6 @@ STATICFILES_DIRS = [BASE_DIR / "static",]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- webradio/views.py
+++ webradio/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
+from .models import Program
+from .forms import ProgramForm
 
 def index(request):
     return render(request, 'index.html')
+
+def programacao(request):
+    return render(request, 'programacao.html')
+
+# Listar os programas na tela de programação
+def programacao(request):
+    programs = Program.objects.all()
+    return render(request, 'programacao.html', {'object_list': programs})
+
+# Create
+def program_create(request):
+    if request.method == "POST":
+        form = ProgramForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('programacao')
+    else:
+        form = ProgramForm()
+    context = {
+        'form': form,
+        'title':'Criar um novo programa'
+    }
+    return render(request, 'form.html', context)
