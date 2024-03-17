67f6833d3e0de2259ffe5dcea446cb3dfbc44051
Thaynix <thaygoncalvesjg@gmail.com>
2024-03-17 19:55:18 -0300

    feat: adiciona create e list dos programas

--- webradio/models.py
+++ webradio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Program(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='imagem')
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    description = models.CharField(max_length=255, verbose_name="Descrição")
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Cadastradado em")
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.name
