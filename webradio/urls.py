2a0ced4e3c32f33089e24b339955ddb537880bc7
Thaynix <thaygoncalvesjg@gmail.com>
2024-03-17 22:47:11 -0300

    feat: adiciona update e delete dos programas
    Também adiciona login e logout no sistema.

--- webradio/urls.py
+++ webradio/urls.py
@@ -2,13 +2,19 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('programacao', programacao, name= 'programacao'),
+    # CRUD
     path('programacao/cadastrar/', program_create, name='program_create'),
+    path('programacao/<int:pk>/atualizar/', program_update, name='program-update'),
+    path('programacao/<int:pk>/deletar/', program_delete, name='program-delete'),
+    # AUTH
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
-] 
-""" + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) """
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webradio/views.py
+++ webradio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Program
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProgramForm
 
 def index(request):
@@ -27,3 +28,34 @@ def program_create(request):
         'title':'Criar um novo programa'
     }
     return render(request, 'form.html', context)
+
+# Update
+def program_update(request, pk):
+    program = get_object_or_404(Program, pk=pk)
+    if request.method == "POST":
+        form = ProgramForm(request.POST, instance=program)
+        if form.is_valid():
+            form.save()
+            return redirect('programacao')
+    else:
+        form = ProgramForm(instance=program)
+    context = {
+        'form': form,
+        'title':  f"Editar o programa {program}"
+    }
+    return render(request, 'form.html', context)
+
+# Delete
+def program_delete(request, pk):
+    program = get_object_or_404(Program, pk=pk)
+    if request.method== 'POST':
+        program.delete()
+        return redirect('programacao')
+    context = {
+        'object': program,
+        'title':f"Deletar o programa {program}",
+        'message':f"Tem certeza que deseja deletar o programa {program}?"
+    }
+    return render(request,'form-delete.html', context)
+
+# Auth
