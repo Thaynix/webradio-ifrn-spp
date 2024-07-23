a94b99c6ae71687f28d9fbe357ee8501cd0fe981
Thaynix <thaygoncalvesjg@gmail.com>
2024-07-23 11:06:08 -0300

    feat: adiciona novas models

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Program, WarningCard
+from .models import Program, WarningCard, ProfileCard, ImageCarousel, AboutRadio
 
 
 # Register your models here.
 admin.site.register(Program)
 admin.site.register(WarningCard)
+admin.site.register(ProfileCard)
+admin.site.register(ImageCarousel)
+admin.site.register(AboutRadio)
 
--- app/models.py
+++ app/models.py
@@ -1,24 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class WarningCard(models.Model):
-    COLOR_OPTIONS = [
-        ('#3a7b50', 'Green'),
-        ('#f8a348', 'Yellow'),
-        ('#e15244', 'Red'),
-        ('#3357FF', 'Blue'),
-    ]
+# Carrosel de imagens
+class ImageCarousel(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='Carousel Imagem')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="publicado em")
 
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return str(self.image)
+
+# Cards de avisos na pagina inicial
+class WarningCard(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    color = models.CharField(max_length=7, choices=COLOR_OPTIONS, default='Green') 
 
     def __str__(self):
         return self.title
 
+class AboutRadio(models.Model):
+    text = models.TextField(unique=True, max_length=1500, verbose_name="Sobre")
+    image = models.ImageField(unique=True, null=True, blank=True, upload_to='images/')
 
+# Cards que mostram a equipe da rádio
+class ProfileCard(models.Model):
+    image = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='imagem')
+    position = models.CharField(max_length=100, verbose_name="Posição")
+    name = models.CharField(max_length=100, verbose_name="Nome")
+    description = models.CharField(max_length=255, verbose_name="Descrição")
+
+    class Meta:
+        ordering = ['-position']
+
+    def __str__(self):
+        return self.name
+
+
+
+# Cards dos programas na pagina de programação
 class Program(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='Imagem')
     name = models.CharField(max_length=100, verbose_name='Nome')
@@ -30,3 +54,18 @@ class Program(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
