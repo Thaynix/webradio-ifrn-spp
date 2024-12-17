e2298d901cbbd67ea5a372adf69e79a2c7abcd15
Thaisy Gonçalves <thaygoncalvesjg@gmail.com>
2024-12-17 13:26:03 -0300

    fix: refaz as migrações

--- app/models.py
+++ app/models.py
@@ -43,9 +43,8 @@ class ProfileCard(models.Model):
 class Pedidos (models.Model):
     singer = models.CharField(null=True,max_length=90, verbose_name="Cantor")
     description = models.CharField(max_length=225, verbose_name="Descrição")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    author = models.CharField(max_length=90, verbose_name="Autor", help_text="Digite seu nominho")
     date = models.DateTimeField(auto_now_add=True, verbose_name="Cadastrado em")
-
     def __str__(self):
         return self.singer
     
