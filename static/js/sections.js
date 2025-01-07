3c6df8a5c1e30e248b83878291bdfa871fc1e74b
Thaisy Gonçalves <thaygoncalvesjg@gmail.com>
2025-01-07 03:03:48 -0300

    feat: adiciona scroll da pagina inicial

--- static/js/sections.js
+++ static/js/sections.js
@@ -1,24 +1,7 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const body = document.body;
-//     const apresentationSection = document.querySelector(".apresentation");
-  
-//     const observer = new IntersectionObserver(
-//       (entries) => {
-//         entries.forEach((entry) => {
-//           if (entry.isIntersecting) {
-//             body.style.backgroundColor = getComputedStyle(apresentationSection).backgroundColor;
-//           } else {
-//             body.style.backgroundColor = ""; // Volta para a cor padrão do body (definida no CSS).
-//           }
-//         });
-//       },
-//       {
-//         threshold: 0.5, // Define a visibilidade mínima da seção (50% visível).
-//       }
-//     );
-  
-//     if (apresentationSection) {
-//       observer.observe(apresentationSection);
-//     }
-//   });
-  
+ function scrollToSection() {
+    const targetSection = document.getElementById('#home');
+    window.scrollTo({
+      top: targetSection.offsetTop,  // posição da seção
+      behavior: 'smooth'             // rolagem suave
+    });
+}
