675a17db7da8a08dbdb69a1df04bafe0b34b3312
Thaisy Gonçalves <thaygoncalvesjg@gmail.com>
2025-01-07 21:58:50 -0300

    cleanup: remove trechos comentados

--- static/js/sections.js
+++ static/js/sections.js
@@ -1,8 +1,5 @@
 function scrollToSection(sectionId) {
-  // Seleciona a seção com o ID fornecido
   const section = document.querySelector(sectionId);
-
-  // Rola até a seção com transição suave
   section.scrollIntoView({
       behavior: 'smooth'
   });
